2a3bd76c2f0aa59196ba1efa125b0eeb2b1bb941
Gino Lucchini FAI2116 <104227944+ginoFAI@users.noreply.github.com>
2025-05-28 20:45:46 -0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://ginoawesome:<db_password>@pruebagalliard.wjqbaic.mongodb.net/?retryWrites=true&w=majority&appName=PruebaGalliard",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
